d03de88113b23fcab9df7cf27101ffb5786e8ddf
brad.martin <brad.martin@aed.pro>
2025-08-01 14:45:05 -0600

    add vector components test

---
+++ test/vector_components_test.dart
@@ -0,0 +1,92 @@
+import 'dart:ffi';
+
+import 'package:opencl/opencl.dart';
+import 'package:test/test.dart';
+
+const sizeOfFloat = 4;
+
+void main() {
+  late List<Platform> platforms;
+  late Context context;
+  late CommandQueue queue;
+
+  setUp(() async {
+    DynamicLibrary libraryCL;
+    try {
+      libraryCL = OpenCL.openDynLib();
+    } catch (e) {
+      throw Exception('could not load OpenCL dynamic library');
+    }
+
+    final cl = OpenCL(libraryCL);
+    platforms = cl.getPlatforms();
+    context = cl.createContext(platforms[0].devices);
+    queue = context.createCommandQueue(platforms[0].devices[0]);
+  });
+
+  test('update position data', () {
+    const vAccessComponents = '''
+__kernel void foo(__global const float2 *pos) {
+  pos->y = pos->x * 2.0f;
+}
+''';
+
+    final posBuf = NativeBuffer(sizeOfFloat * 2);
+    final posList = posBuf.byteBuffer.asFloat32List();
+    posList[0] = 2.0;
+
+    final posMem = context.createBuffer(sizeOfFloat * 2,
+        hostData: posBuf, kernelWrite: true, hostRead: true);
+
+    final vAccessProg = context.createProgramWithSource([vAccessComponents]);
+    final buildLog = <String>[];
+    vAccessProg.buildProgram(platforms[0].devices, '', buildLog);
+    final vAccessKernel = vAccessProg.createKernel('foo')
+      ..setKernelArgMem(0, posMem);
+
+    queue
+      ..enqueueNDRangeKernel(vAccessKernel, 1,
+          globalWorkSize: [1], localWorkSize: [1])
+      ..enqueueReadBuffer(posMem, 0, posBuf.size, posBuf, blocking: true)
+      ..flush()
+      ..finish()
+      ..release();
+    vAccessKernel.release();
+    vAccessProg.release();
+
+    final pos = posBuf.byteBuffer.asFloat32List();
+    expect(pos[0] * 2.0 == pos[1], true);
+
+    posMem.release();
+    posBuf.free();
+  });
+
+  test('illegal access position data', () {
+    const vAccessComponents = '''
+__kernel void foo(__global const float2 *pos) {
+  float invalid = pos->z;
+}
+''';
+
+    final posBuf = NativeBuffer(sizeOfFloat * 2);
+
+    final posMem = context.createBuffer(sizeOfFloat * 2,
+        hostData: posBuf, kernelWrite: true, hostRead: true);
+
+    final vAccessProg = context.createProgramWithSource([vAccessComponents]);
+    final buildLog = <String>[];
+    vAccessProg.buildProgram(platforms[0].devices, '', buildLog);
+
+    var ok = true;
+    try {
+      vAccessProg.createKernel('foo').release();
+    } catch (e) {
+      ok = false;
+    }
+    expect(ok, false);
+
+    vAccessProg.release();
+    posMem.release();
+    posBuf.free();
+  });
+}
